a3b3e17b0d2b215c86a5d52b70fb328051acc8e1
Zamir <baizakoff2017@gmail.com>
2020-06-06 17:03:38 +0300

    init

---
+++ script/script-hw.js
@@ -0,0 +1,50 @@
+/*
+Домашнее задание
+Реализовать функцию, которая рекурсивно ищет сотрудника по зп.
+Зп сотрудника не должна превышать указанное значение и должна быть ближайшей, нежели у других сотрудников.
+*/
+
+
+function addWorker(name, lastname, salary){
+	return {name, lastname, salary, staff: []}
+}
+/*
+Иванов Иван
+	Петров Петр
+		Антонов Антон
+		Максимов Максим
+	Сидоров Сидор
+		Ушанов Анатолий
+	Романова Анна	
+		Грибов Максим
+		Куликов Антон
+			Сергеев Сергей
+*/
+let boss = addWorker('Иван', 'Иванов', 1000000)
+
+boss.staff.push(addWorker('Петр',  'Петров',   100000))
+boss.staff.push(addWorker('Сидор', 'Сидоров',  100000))
+boss.staff.push(addWorker('Анна',  'Романова', 100000))
+
+boss.staff[0].staff.push(addWorker('Антон',  	'Антонов',  10000))
+boss.staff[0].staff.push(addWorker('Максим', 	'Максимов', 10000))
+boss.staff[1].staff.push(addWorker('Анатолий',  'Ушанов',  10000))
+boss.staff[2].staff.push(addWorker('Максим',  	'Грибов',  10000))
+boss.staff[2].staff.push(addWorker('Антон',  	'Куликов',  10000))
+
+boss.staff[2].staff[1].staff.push(addWorker('Сергей',  	'Сергеев',  1000))
+
+function print(worker, pref){
+	console.log(`${pref}имя: ${worker.name}`)
+	console.log(`${pref}фамилия: ${worker.lastname}`)
+	console.log(`${pref}зарплата: ${worker.salary}`)
+	console.log(`${pref}----------------`)
+}
+
+function render(node=boss, pref=''){
+	print(node, pref)
+	for (let w of node.staff){
+		render(w, pref+'    ')
+	}
+}
+render();
